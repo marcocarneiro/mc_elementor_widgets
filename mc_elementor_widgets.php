5f0a187ec39d8beab84872488b6c3b30b22e8a8a
marcocarneiro <marco.carneiro7@gmail.com>
2022-10-28 17:24:43 -0300

    Create mc_elementor_widgets.php

---
+++ mc_elementor_widgets.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name: MC Elementor Widgets
+Plugin URI: https://marco-carneiro.com.br/
+Description: Componentes do Elementor para adicionar aos componentes já instalados.
+Version: 1.0
+Author: Marco Carneiro - Lima Digital
+Author URI: https://marco-carneiro.com.br/
+License: GPLv2 or later
+Text Domain: mc_elementor_widgets
+*/
