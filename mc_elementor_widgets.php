af15188630305a44041af8ee0e15cf05166f38d9
marcocarneiro <marco.carneiro7@gmail.com>
2022-10-31 23:46:57 -0300

    Update plugin

--- mc_elementor_widgets.php
+++ mc_elementor_widgets.php
@@ -1,10 +1,22 @@
+<?php
 /*
 Plugin Name: MC Elementor Widgets
 Plugin URI: https://marco-carneiro.com.br/
 Description: Componentes do Elementor para adicionar aos componentes já instalados.
-Version: 1.0
+Version: 2.0
 Author: Marco Carneiro - Lima Digital
 Author URI: https://marco-carneiro.com.br/
 License: GPLv2 or later
 Text Domain: mc_elementor_widgets
 */
+
+//INCLUDES
+$arqCoreInit = plugin_dir_path( __FILE__ ). 'core_init.php';
+
+//Aborta se não for executado pelo WP
+if( ! defined('WPINC')){die();}
+
+if(file_exists($arqCoreInit)){
+    require_once($arqCoreInit);
+}
+
